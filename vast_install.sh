abaaa16f54adb89880ba6fef8b8a5c2b872a878e
Derrick Schultz <dvsmethid@gmail.com>
2019-09-02 17:11:54 -0400

    Update vast_install.sh

--- vast_install.sh
+++ vast_install.sh
@@ -61,3 +61,15 @@ cd ../
 #7:
 #8:https://drive.google.com/open?id=1SNmg5DzSGRkaTCMo9mXc3h2Z-bVpPN80
 #9:https://drive.google.com/open?id=1w3mXP4HROsFM3NkgM0KyEuzqy3FGHV-6
+
+#w
+mkdir w
+cd w
+gdown --id 19OcfDckELZz8lGnOYs-miQh31L5Mfprr --output w-r02.tfrecords
+gdown --id 1YPrzAGJF9HOgWpof0Sm8xntSEvYTjuae --output w-r03.tfrecords
+gdown --id 1JiQtHVxlzVGr7VyJclSxKVnVO5lPcbZe --output w-r04.tfrecords
+gdown --id 1LDFfOogH1X-32v83G5QkwmHkG64w6zKl --output w-r05.tfrecords
+gdown --id 1S-b0SDs5oA0gcvOabPq3CL2nFieBdwM9 --output w-r06.tfrecords
+gdown --id 1IO6KSUwXZl6AfgNwKJl9oesDyUUxco-B --output w-r07.tfrecords
+gdown --id 1usEUOykORDTj0VolBxr1MlRNP_egpwsD --output w-r08.tfrecords
+gdown --id 1huTQOqImhxaonwW-WxzrpezLwmq_k0AT --output w-r09.tfrecords
